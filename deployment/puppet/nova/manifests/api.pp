02f334eb88e0f4702602f7bd4c68fec76a4881c9
Andrey Shestakov <ashestakov@mirantis.com>
2014-03-05 17:15:21 +0200

    Set osapi_compute_workers to number of processors
    Backport from 4.1

--- deployment/puppet/nova/manifests/api.pp
+++ deployment/puppet/nova/manifests/api.pp
@@ -27,6 +27,7 @@ class nova::api(
   $nova_quota_driver = 'nova.quota.NoopQuotaDriver',
   $nova_rate_limits  = undef,
   $nova_user_password= undef, #Empty password generates error and saves from non-working installation
+  $api_workers       = $::processorcount,
 ) {
 
   include nova::params
@@ -93,6 +94,7 @@ class nova::api(
     'DEFAULT/enabled_apis':     value => $enabled_apis;
     'DEFAULT/volume_api_class': value => $volume_api_class;
     'DEFAULT/keystone_ec2_url': value => "http://${auth_host}:5000/v2.0/ec2tokens";
+    'DEFAULT/osapi_compute_workers': value => $api_workers;
     'DEFAULT/quota_driver':     value => $nova_quota_driver;
   }
   nova_config {
